9f37ab057f0c81f1277f89ee72b76aedecb5dd9d
canh <canhphan281094@gmail.com>
2020-03-28 09:37:51 +0700

    Updates

---
+++ precache-manifest.67cdf507b6293d248a221932d803f192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50d7aef6b7b1a40cc90f91d9ccecaf4",
+    "url": "/react-deloy/index.html"
+  },
+  {
+    "revision": "ce39a641bb8112bccb89",
+    "url": "/react-deloy/static/css/2.d5abd7ea.chunk.css"
+  },
+  {
+    "revision": "0c1c52c3cc5a9864e474",
+    "url": "/react-deloy/static/css/main.6d69dbc2.chunk.css"
+  },
+  {
+    "revision": "ce39a641bb8112bccb89",
+    "url": "/react-deloy/static/js/2.2b4ed9ac.chunk.js"
+  },
+  {
+    "revision": "486c864dbeea4f82b0912c66e3826be4",
+    "url": "/react-deloy/static/js/2.2b4ed9ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1c52c3cc5a9864e474",
+    "url": "/react-deloy/static/js/main.2abf8290.chunk.js"
+  },
+  {
+    "revision": "3b4dd6ccd4521fbae5a9",
+    "url": "/react-deloy/static/js/runtime-main.af23d443.js"
+  }
+]);
